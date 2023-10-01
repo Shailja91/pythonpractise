3710137ad1ae478dfda871a672f2d572060cb688
Shailja91 <sharmashailja91@gmail.com>
2023-10-01 06:24:12 +0530

    Please enter the commit message for your changes. Lines starting

--- FoodOrderingApp.py
+++ FoodOrderingApp.py
@@ -1,18 +1,28 @@
-Menu={"FoodItems":"Price in Rs.",
-	  "veg.pulao":100,
-	  "plain rice":80,
-	  "jeera rice":90,
-	  "dal fry":100,
-	  "chapati":10,
-	  "naan":20,
-	  "paneer":110}
+Menu=[[{"0 FoodItems":"Price in Rs."}],
+	   {"1 veg.pulao":100},
+	   {"2 plain rice":80},
+	   {"3 jeera rice":90},
+	   {"4 dal fry":100},
+	   {"5 chapati":10},
+	   {"6 naan":20},
+	   {"7 paneer":110}]
 
-for key,value in Menu.items(): #printing Menu vertically
-	print(key,"		",":",value)
+for num in range(0,len(Menu)): #printing Menu vertically
+	for key,values in Menu.items:
+		print(num, key, ":", values)
 	Tax = 0.07
 	cart={} #empty cart
 
 	def addtocart(key,value):
+		item = 0
+		while item<=7:
+			dish = int(input("Enter the number of dish from the menu"))
+			print(dish)
+			item +=1
+		else:
+			print("sorry! you have entered wrong choice.")
+
+
 
 
 
---
+++ secretcode.py
@@ -0,0 +1,22 @@
+import random
+import string
+
+str=input("Enter the string")
+coding=input(" enter 0 for coding,1 for decoding")
+if(coding == "0"):
+	coding=True
+elif(coding=="1"):
+	coding=False
+if(coding == True):
+	if len(str)>=3:
+		print(str)
+		a=random.choice(string.ascii_letters)+random.choice(string.ascii_letters)+random.choice(string.ascii_letters)
+		str=a + str[::-1] + a
+		print(str)
+	else:
+		print(str[::-1])
+else:
+	if len(str)>=3:
+		str=str[::-1]
+		str=str[3:-3]
+		print(str)
